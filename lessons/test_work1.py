9f40c4347d8b3dfd7bb978c088d5983da182c706
shiro-kreishi <vadas25@yandex.ru>
2024-05-27 09:28:35 +0500

    add test work

---
+++ lessons/test_work1.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Загрузка и предобработка данных
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(-1, 28 * 28).astype('float32') / 255
+x_test = x_test.reshape(-1, 28 * 28).astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Создание модели
+model = Sequential([
+    Dense(512, activation='relu', input_shape=(28 * 28,)),
+    Dense(10, activation='softmax')
+])
+
+# Компиляция модели
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Обучение модели
+model.fit(x_train, y_train, epochs=5, batch_size=32, validation_split=0.2)
+
+# Оценка модели
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {test_acc}')
